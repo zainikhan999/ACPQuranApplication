ec17f9ed21d1da1a94cf15e8a4f08e0a86422243
zainikhan999 <zk.032918@gmail.com>
2024-10-31 00:11:44 +0500

    footer error resolved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,7 +38,7 @@ export default function Footer() {
             rel="noopener noreferrer"
             className="flex items-center gap-2 text-[#FFFFFF]"
           >
-            <FaWhatsapp size={24} '/>
+            <FaWhatsapp size={24} />
             <span>+123 456 7890</span>
           </a>
           
